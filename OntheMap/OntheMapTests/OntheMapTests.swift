2d4f704d0463b3c2ea279ce15152deaeecd9888c
Unknown <sergiosrgtc@gmail.com>
2018-06-28 22:23:32 -0300

    Initial commit

---
+++ OntheMap/OntheMapTests/OntheMapTests.swift
@@ -0,0 +1,36 @@
+//
+//  OntheMapTests.swift
+//  OntheMapTests
+//
+//  Created by Sergio Costa on 28/06/18.
+//  Copyright © 2018 Sergio Costa. All rights reserved.
+//
+
+import XCTest
+@testable import OntheMap
+
+class OntheMapTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
